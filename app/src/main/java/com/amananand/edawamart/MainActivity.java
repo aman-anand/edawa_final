dad372a0f6281e0d6542e7de91cba34e030da214
aman anand <aman.anandx3@gmail.com>
2017-12-23 16:03:57 +0530

    implemented camera opion while uploading order image

--- app/src/main/java/com/amananand/edawamart/MainActivity.java
+++ app/src/main/java/com/amananand/edawamart/MainActivity.java
@@ -10,15 +10,22 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -29,25 +36,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int MEDIA_TYPE_IMAGE = 1;
     private static final int GALLERY_CAPTURE_IMAGE_REQUEST_CODE = 34225;
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 34227;
+    private static String IMAGE_DIRECTORY_NAME="Order Image";
     WebView vistaWeb;
+    String data = "";
+    String str;
+    CookieManager cookieManager;
     private WebView webView;
-    String data="";
     private Context context;
     private ProgressDialog progressDialog;
     private ConstraintLayout rootLayout;
-    String str;
-    CookieManager cookieManager;
+    private Uri fileUri;
+
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this;
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
         rootLayout = findViewById(R.id.rootLayout);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             HandlePermission.requestAllPermission((Activity) context);
@@ -55,12 +72,10 @@ public class MainActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(context);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.setTitle("Image Upload in progress");
-        cookieManager=CookieManager.getInstance();
+        cookieManager = CookieManager.getInstance();
         cookieManager.setAcceptCookie(true);
 
         initWebView();
-
-
 
 
 //        if (vistaWeb.getUrl().equals("http://edawamart.com/orders/upload-order-image")) {
@@ -122,29 +137,63 @@ public class MainActivity extends AppCompatActivity {
                         uploadImageToServer(new File(imagePath));
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
+                        gohome();
                     }
                 } else {
                     Toast.makeText(this, "Error getting image", Toast.LENGTH_SHORT).show();
+                    gohome();
                 }
             } else {
                 Toast.makeText(getApplicationContext(),
                         "Sorry! gallery operation Failed.", Toast.LENGTH_SHORT)
                         .show();
+                gohome();
+            }
+        }else if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                String imagePath = fileUri.getPath();
+                if (!TextUtils.isEmpty(imagePath)) {
+                    try {
+                        uploadImageToServer(new File(imagePath));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                        gohome();
+                    }
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+                gohome();
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+                gohome();
             }
         }
     }
 
+    private void gohome() {
+        vistaWeb.loadUrl("http://edawamart.com");
+    }
+
     private void uploadImageToServer(File file) throws MalformedURLException {
+        progressDialog.setCancelable(false);
         progressDialog.show();
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
         RequestParams requestParams = new RequestParams();
         try {
             requestParams.put("order_image", file, "image/png");
-            requestParams.put("data",data);
-            requestParams.put("d","android");
+            requestParams.put("data", data);
+            requestParams.put("d", "android");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             Toast.makeText(this, "File Not Found", Toast.LENGTH_SHORT).show();
+            gohome();
         }
         asyncHttpClient.post(this, "http://edawamart.com/orders/do_upload_order_image_android", requestParams,
                 new AsyncHttpResponseHandler() {
@@ -153,13 +202,13 @@ public class MainActivity extends AppCompatActivity {
                         AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
                         progressDialog.cancel();
                         try {
-                            str=new String(responseBody,"utf-8");
+                            str = new String(responseBody, "utf-8");
                         } catch (UnsupportedEncodingException e) {
                             e.printStackTrace();
                         }
                         alertDialog.setTitle("Success")
                                 .setMessage("Image Uploaded Successfully!")
-                                .setCancelable(true)
+                                .setCancelable(false)
                                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
@@ -167,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 })
                                 .show();
-                        vistaWeb.loadUrl("http://edawamart.com");
+                       gohome();
 
                         rootLayout.setBackgroundResource(R.color.transparent);
 
@@ -189,7 +238,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 })
                                 .show();
-                        vistaWeb.loadUrl("http://edawamart.com");
+                        gohome();
                         rootLayout.setBackgroundResource(R.color.transparent);
 
                     }
@@ -210,10 +259,83 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (vistaWeb.canGoBack()){
+        if (vistaWeb.canGoBack()) {
             vistaWeb.goBack();
-        }else{
+        } else {
             super.onBackPressed();
+        }
+    }
+
+    private void getImageFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+    public Uri getOutputMediaFileUri(int type) {
+//        return Uri.fromFile(getOutputMediaFile(type));
+//        System.out.println(Uri.fromFile(getOutputMediaFile(type)));
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private void getImageFromGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (HandlePermission.checkStoragePerms((Activity) context)) {
+                Intent intent = new Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(Intent.createChooser(intent, "Choose Picture"), GALLERY_CAPTURE_IMAGE_REQUEST_CODE);
+            } else {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+                alertDialog.setTitle("ERROR")
+                        .setMessage("Permission not granted, cannot do this task")
+                        .setCancelable(true)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+                rootLayout.setBackgroundResource(R.color.transparent);
+
+
+            }
         }
     }
 
@@ -221,33 +343,47 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             if (url.contains("http://edawamart.com/orders/upload-order-image")) {
-                String[] urlData=url.split("\\?data=");
-                data=urlData[1];
-                rootLayout.setBackgroundResource(R.color.black);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    if (HandlePermission.checkStoragePerms((Activity) context)) {
-                        Intent intent = new Intent(
-                                Intent.ACTION_PICK,
-                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                String[] urlData = url.split("\\?data=");
+                data = urlData[1];
+                final AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog);
 
-                        startActivityForResult(Intent.createChooser(intent, "Choose Picture"), GALLERY_CAPTURE_IMAGE_REQUEST_CODE);
-                    } else {
-                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-                        alertDialog.setTitle("ERROR")
-                                .setMessage("Permission not granted, cannot do this task")
-                                .setCancelable(true)
-                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                })
-                                .show();
-                        rootLayout.setBackgroundResource(R.color.transparent);
+                TextView gallery, camera, cancel;
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View v = inflater.inflate(R.layout.dialog01, null);
+                gallery = v.findViewById(R.id.gallery);
+                camera = v.findViewById(R.id.camera);
+                cancel = v.findViewById(R.id.cancel);
+                builder.setView(v)
+                        .setCancelable(false);
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
 
-
+                gallery.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getImageFromGallery();
+                        alertDialog.cancel();
                     }
-                }
+                });
+                camera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getImageFromCamera();
+                        alertDialog.cancel();
+                    }
+                });
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.cancel();
+                        vistaWeb.loadUrl("http://edawamart.com");
+                    }
+                });
+
+
+                rootLayout.setBackgroundResource(R.color.black);
+
+
             } else {
                 return super.shouldOverrideUrlLoading(view, url);
             }
